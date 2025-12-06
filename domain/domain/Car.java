320d937345b1bf5501eafc67697792ed09d52ebf
Masssry92 <151083381+Masssry92@users.noreply.github.com>
2025-12-06 19:21:21 +0100

    car

--- domain/domain/Car.java
+++ domain/domain/Car.java
@@ -1,18 +1,50 @@
 package domain;
 
 public class Car extends Vehicle {
-    private String boite;
 
-    public Car(String plaque, String marque, String boite) {
-        super(plaque, marque);
-        this.boite = boite;
+    private final String gearbox;
+
+    private Car(Builder builder) {
+        super(builder.plate, builder.brand);
+        this.gearbox = builder.gearbox;
     }
 
-    public String getBoite() { return boite; }
-    public void setBoite(String boite) { this.boite = boite; }
+    public String getGearbox() {
+        return gearbox;
+    }
 
     @Override
     public String toString() {
-        return "Car{" + super.toString() + ", boite='" + boite + "'}";
+        return "Car{" +
+                "plate='" + getPlate() + '\'' +
+                ", brand='" + getBrand() + '\'' +
+                ", gearbox='" + gearbox + '\'' +
+                '}';
+    }
+
+
+    public static class Builder {
+        private String plate;
+        private String brand;
+        private String gearbox;
+
+        public Builder setPlate(String plate) {
+            this.plate = plate;
+            return this;
+        }
+
+        public Builder setBrand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        public Builder setGearbox(String gearbox) {
+            this.gearbox = gearbox;
+            return this;
+        }
+
+        public Car build() {
+            return new Car(this);
+        }
     }
 }
