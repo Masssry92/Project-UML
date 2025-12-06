87e9aec5c044f8cfe88c92d8bd53c080bdac670e
Masssry92 <151083381+Masssry92@users.noreply.github.com>
2025-12-06 19:31:41 +0100

    modif customer

--- domain/domain/Customer.java
+++ domain/domain/Customer.java
@@ -1,24 +1,55 @@
 package domain;
 
 public class Customer {
-    private int id;
-    private String name;
-    private Vehicle vehicle;
 
-    public Customer(int id, String name, Vehicle vehicle) {
-        this.id = id;
-        this.name = name;
-        this.vehicle = vehicle;
+    private final int id;
+    private final String name;
+    private final Vehicle vehicle;
+
+    private Customer(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.vehicle = builder.vehicle;
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public int getId() {
+        return id;
+    }
 
-    public Vehicle getVehicle() { return vehicle; }
-    public void setVehicle(Vehicle vehicle) { this.vehicle = vehicle; }
+    public String getName() {
+        return name;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+
+    public static class Builder {
+        private int id;
+        private String name;
+        private Vehicle vehicle;
+
+        public Builder setId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setVehicle(Vehicle vehicle) {
+            this.vehicle = vehicle;
+            return this;
+        }
+
+        public Customer build() {
+            return new Customer(this);
+        }
+    }
 
     @Override
     public String toString() {
