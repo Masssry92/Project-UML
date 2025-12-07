69d15c54452f87f1ffe514765f640365c7cf9a39
Masssry92 <151083381+Masssry92@users.noreply.github.com>
2025-12-07 23:58:58 +0100

    update

--- factory/factory/GarageFactory.java
+++ factory/factory/GarageFactory.java
@@ -5,6 +5,7 @@ import domain.Garage;
 public class GarageFactory {
 
     public static Garage createGarage(int id, String name) {
-        return new Garage(id, name);
+        Garage garage = new Garage(id, name);
+        return garage;
     }
 }
