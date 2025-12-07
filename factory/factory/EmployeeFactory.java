b1f0ac1bd82f2201306d044e7b77bd2ac7ec5f6e
Masssry92 <151083381+Masssry92@users.noreply.github.com>
2025-12-07 23:56:04 +0100

    modif employee

--- factory/factory/EmployeeFactory.java
+++ factory/factory/EmployeeFactory.java
@@ -1,10 +1,16 @@
 package factory;
 
 import domain.Employee;
+import domain.Garage;
 
 public class EmployeeFactory {
 
-    public static Employee createEmployee(int id, String name, String competence) {
-        return new Employee(id, name, competence);
+    public static Employee createEmployee(int id, String name, String competency, Garage garage) {
+
+        Employee employee = new Employee(id, name, competency);
+
+        garage.addEmployee(employee);
+
+        return employee;
     }
 }
